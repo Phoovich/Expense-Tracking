fa411f6b61ef9a2e9d8d2ee66c82eb9737c53b7c
Phoovich <Phoovich@gmail.com>
2026-01-04 19:47:02 +0700

    query cat by family_id and kind

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 import app.models
 from app.routers.family import router as family_router
 from app.routers.transaction import router as tx_router
+from app.routers.category import router as cat_router
 
 app = FastAPI(title="Expense API")
 
 app.include_router(tx_router)
 app.include_router(family_router)
+app.include_router(cat_router)
 
 
 @app.get("/", tags=["Health"])
